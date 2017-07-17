b763112d4deb5fe0332dbe8b2b24ab9c44573ea8
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-17 19:39:25 -0400

    Added default meal bar on meals page

--- client/assets/js/controllers/resMealsCtrl.js
+++ client/assets/js/controllers/resMealsCtrl.js
@@ -5,16 +5,22 @@ var resMealsCtrl = function ($scope,commonService, mealService, modalService) {
   var restaurant = commonService.getRestaurantID();
 
   $scope.meals = [];
+  $scope.defaultMealForm = {}
 
   mealService
     .getMeals({ restaurant_id: restaurant })
     .success(function(data, status, headers, config) {
       $scope.meals = data;
+      $scope.defaultMealForm.meals = $scope.meals[0]
     })
     .error(function(data, status, headers, config) {
       // Handle login errors here
       $scope.message = 'Error: Something Went Wrong';
     });
+
+  $scope.changeDefaultMeal = (meal) => {
+    console.log('meal', meal)
+  }
 
 };
 
